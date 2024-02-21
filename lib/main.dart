1f247dcfdb1ca58df3988d1b148565150c5a94c3
SuperSlowStarter <cleangabby@gmail.com>
2024-02-22 04:36:11 +0900

    Update for learn State

--- lib/main.dart
+++ lib/main.dart
@@ -12,67 +12,67 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
+  List<int> numbers = [];
+  bool showTitle = true;
 
-  void onClicked() {
+  void toggleTitle() {
     setState(() {
-      counter += 1;
-    });
-  }
-
-  void decrease() {
-    setState(() {
-      counter -= 1;
-    });
-  }
-
-  void initialize() {
-    setState(() {
-      counter = 0;
+      showTitle = !showTitle;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(color: Colors.red),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
-              ),
-              Text(
-                '$counter',
-                style:
-                    const TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: onClicked,
-                    icon: const Icon(Icons.add_box_rounded),
-                    iconSize: 60,
-                  ),
-                  IconButton(
-                    onPressed: decrease,
-                    icon: const Icon(Icons.indeterminate_check_box),
-                    iconSize: 60,
-                  ),
-                  IconButton(
-                    onPressed: initialize,
-                    icon: const Icon(Icons.check_box),
-                    iconSize: 60,
-                  ),
-                ],
-              ),
+              showTitle ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void dispose() {
+    super.dispose();
+    print('disposed!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build!');
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 40,
+        fontWeight: FontWeight.w500,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
